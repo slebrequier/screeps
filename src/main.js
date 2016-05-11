5f81ae9d87e820a89ced6b25d4a47990ba47b95f
Stéphane Lebréquier <stephane.lebrequier@aramisauto.com>
2016-05-11 15:31:05 +0200

    [screeps] Ajout role builder

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
 
-        if (creep.memory.role == 'harvester') {
+        if(creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
         }
 
-        if (creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
         }
     }
 }

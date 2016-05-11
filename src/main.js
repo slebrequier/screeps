32ec6f19d2047938961414b5fc3bfd5c6832c291
Stéphane Lebréquier <stephane.lebrequier@aramisauto.com>
2016-05-11 15:39:50 +0200

    [screeps] Mise a jour role harvester

--- src/main.js
+++ src/main.js
@@ -2,14 +2,19 @@ var roleHarvester = require('role.harvester');
 var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
+
+    for (var name in Game.rooms) {
+        console.log('Room "' + name + '" has ' + Game.rooms[name].energyAvailable + ' energy');
+    }
+
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
 
-        if(creep.memory.role == 'harvester') {
+        if (creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
         }
 
-        if(creep.memory.role == 'builder') {
+        if (creep.memory.role == 'builder') {
             roleBuilder.run(creep);
         }
     }
--- src/role.harvester.js
+++ src/role.harvester.js
@@ -1,15 +1,24 @@
 var roleHarvester = {
     /** @param {Creep} creep **/
-    run: function(creep) {
+    run: function (creep) {
         if (creep.carry.energy < creep.carryCapacity) {
             var sources = creep.room.find(FIND_SOURCES);
 
             if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
             }
-        } else if (Game.spawns.Spawn1.energy < Game.spawns.Spawn1.energyCapacity) {
-            if (creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns.Spawn1);
+        } else {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                        structure.energy < structure.energyCapacity;
+                }
+            });
+
+            if (targets.length > 0) {
+                if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
             }
         }
     }

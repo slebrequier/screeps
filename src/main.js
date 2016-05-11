fe92cd101ccabdcde227c5b828985d829f439111
Stéphane Lebréquier <stephane.lebrequier@aramisauto.com>
2016-05-11 15:11:05 +0200

    [screeps] Tutorial 2

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
 
-        roleHarvester.run(creep);
+        if (creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+
+        if (creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
     }
 }

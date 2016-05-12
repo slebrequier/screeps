c68737637d04e92d184601391a94fd54f7c8e466
Stéphane Lebréquier <stephane.lebrequier@aramisauto.com>
2016-05-12 13:19:37 +0200

    [screeps] Tutorial 4

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 var roleHarvester = require('role.harvester');
-var roleBuilder = require('role.builder');
+var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
-    for (var name in Game.rooms) {
-        console.log('Room "' + name + '" has ' + Game.rooms[name].energyAvailable + ' energy');
+    for (var name in Memory.creeps) {
+        if (!Game.creeps[name]) {
+            delete Memory.creeps[name];
+        }
+    }
+
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+
+    console.log('Harvesters: ' + harvesters.length);
+
+    if (harvesters.length < 2) {
+        var newName = Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE], undefined, {role: 'harvester'});
+        console.log('Spawning new harvester: ' + newName);
     }
 
     for (var name in Game.creeps) {
@@ -13,8 +24,8 @@ module.exports.loop = function () {
             roleHarvester.run(creep);
         }
 
-        if (creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
+        if (creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
         }
     }
 }
--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -1,9 +1,8 @@
 var roleUpgrader = {
     /** @param {Creep} creep **/
-    run: function(creep) {
+    run: function (creep) {
         if (creep.carry.energy == 0) {
             var sources = creep.room.find(FIND_SOURCES);
-
             if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
             }
